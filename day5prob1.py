fac3e716452b21de8e8db77ba1a00decaeb3fabd
Aanandita Diwan <aananditadiwan@gmail.com>
2022-12-05 12:57:57 +0530

    Add files via upload

---
+++ day5prob1.py
@@ -0,0 +1,40 @@
+import re 
+
+one = ['C','S','G','B']
+two = ['G','V','N','J','H','W','M','T']
+three = ['S','Q','M']
+four = ['M','N','W','T','L','S','B']
+five = ['P','W','G','V','T','F','Z','J']
+six = ['S','H','Q','G','B','T','C']
+seven = ['W','B','P','J','T']
+eight = ['M','Q','T','F','Z','C','D','G']
+nine = ['F','P','B','H','S','N']
+
+containers = [one,two,three,four,five,six,seven,eight,nine]
+
+sub_list = ["move","from","to"]
+
+file = open('input.txt').read()
+contents=file.split('\n')
+
+for x in contents:
+
+    for sub in sub_list:
+        x = x.replace( sub , '')
+    x = re.findall(r'\d+', x)
+
+    fromStack = int(x[1])
+    movingContainer = int(x[0])
+    toStack = int(x[2])
+
+    if(movingContainer<2) :
+        containers[toStack-1].insert(0, containers[fromStack-1][movingContainer-1])
+        del containers[fromStack-1][0]
+
+    else :
+        for counter in range (0,movingContainer):
+            containers[toStack-1].insert(0, containers[fromStack-1][0])
+            del containers[fromStack-1][0] 
+            
+for counter in containers:
+    print(counter[0])      
